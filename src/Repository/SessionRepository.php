fef305b638d0966d72cba5570a02933fd419cf5d
Dariel Vicedo <darielvicedo@gmail.com>
2024-02-16 09:10:46 -0500

    improvement: Descending finished sessions

--- src/Repository/SessionRepository.php
+++ src/Repository/SessionRepository.php
@@ -49,32 +49,8 @@ class SessionRepository extends ServiceEntityRepository
             ])
             ->where('s.createdAt BETWEEN :start AND :end')
             ->andWhere('s.finishedAt IS NOT NULL')
+            ->orderBy('s.finishedAt', 'DESC')
             ->getQuery()
             ->getResult();
     }
-
-//    /**
-//     * @return Session[] Returns an array of Session objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Session
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
